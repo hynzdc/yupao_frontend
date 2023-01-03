8f08591db41b73fb4398d036fe710bd22849d341
郝亚宁 <austinstd@163.com>
2023-01-03 20:08:52 +0800

    add:基本框子已经搭建好了

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Vant from 'vant';
+import 'vant/lib/index.css';
+const app = createApp(App)
+app.use(Vant)
+app.mount('#app')
